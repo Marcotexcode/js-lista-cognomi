3ab8626b34141c07afb7439b36f277f4e9da78e8
Marco Tessitore <marcotexcode@gmail.com>
2021-05-31 14:22:55 +0200

    add: script

--- js/script.js
+++ js/script.js
@@ -6,3 +6,24 @@
 //  3.  stampa un messaggio appropriato sull’esito del controllo.
 
 
+
+// Chiedi all’utente la sua email
+
+var mailUtente = prompt('Inserisci la tua mail: ');
+
+
+var listaMail = ['marco@gmail.com' ,  'giovanni@gmail.com' , 'giacomo@gmail.com' , 'davide@gmail.com' , 'sara@gmail.com' ];
+
+
+//  2.  controlla che sia nella lista di chi può accedere,
+
+for ( i = 0; listaMail.length < 5; i++ ){
+
+    if (listaMail[i].includes(mailUtente)) {
+        console.log('La tua mail e gia presente');
+    } else {
+        console.log('La tua mail non e presente');
+    }
+}
+
+
