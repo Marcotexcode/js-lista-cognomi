214fcea1408e7c9fc8982a7b26364923e917e2bf
Marco Tessitore <marcotexcode@gmail.com>
2021-07-10 21:55:16 +0200

    mod

--- cognomi/js/script.js
+++ cognomi/js/script.js
@@ -7,18 +7,21 @@
 // 4- scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
 
 function capitalizeFirstLetter(string) {
+
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+
+}
   
 
 // 1- chiedi all’utente il cognome
 
 var cognomeUtente = capitalizeFirstLetter(prompt('Inserisci il tuo cognome: '));
-var posizione = false;
+var posizione;
 
 // 2- inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
 
 var listaCognomi = ['Bianchi' , 'Neri' , 'Rossi' , 'Verdi' , 'Gialli'];
+
 listaCognomi.push(cognomeUtente);
 
 // 3- stampa la lista ordinata alfabeticamente
@@ -27,16 +30,32 @@ listaCognomi.sort();
 
 
 for(var i = 0; i < listaCognomi.length; i++){
+
     console.log(listaCognomi[i]);
-}
 
-// 4- scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
-
-for(var i = 0; i < listaCognomi.length; i++){
+    // 4- scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
     if(listaCognomi[i] === cognomeUtente) {
+
         posizione = i + 1;
+
     }
 }
 
 console.log(posizione + ' ' + cognomeUtente);
 
+
+// seconda soluzione
+
+var i =  0;
+
+while(cognomeUtente  != listaCognomi[i]){
+
+    i++;
+
+}
+
+console.log(++i + ' ' + cognomeUtente);
+
+// Spiegazione nell ultimo for per poter stampare il cognome il numero di dove e messo il cognome messo dall' utente devo dire nell if che nella listaCognomi[i] diventa uguale a cognomeUtente
+// devo creare una variabile in cui gli viene dato il risultato dell if che sarebbe il dato della i quando diventa uguale all cognomeUtente il +1 serve per non farlo partire da 0 ma farlo partire 
+// direttamente da 1.
--- mail/js/script.js
+++ mail/js/script.js
@@ -7,52 +7,53 @@
 
 
 
-// Chiedi all’utente la sua email
+// // Chiedi all’utente la sua email
 
     var mailUtente = prompt('Inserisci la tua mail: ');
 
     var trovato = false;
-
     var listaMail = ['marco@gmail.com' ,  'giovanni@gmail.com' , 'giacomo@gmail.com' , 'davide@gmail.com' , 'sara@gmail.com' ];
 
 
-// // //  2.  controlla che sia nella lista di chi può accedere,
+// // // //  2.  controlla che sia nella lista di chi può accedere,
 
-    for(var i = 0; i < listaMail.length; i++) {
+//     for(var i = 0; i < listaMail.length; i++) {
 
-// // //  3.  stampa un messaggio appropriato sull’esito del controllo.
-        if (listaMail[i] === mailUtente) {
-            trovato = true;
-            console.log('La tua mail e gia presente');
-        }
-    }
+// // // //  3.  stampa un messaggio appropriato sull’esito del controllo.
+//         if (listaMail[i] === mailUtente) {
+//             trovato = true;
+//             console.log('La tua mail e gia presente');
+//         }
+//     }
 
-// // //  3.  stampa un messaggio appropriato sull’esito del controllo.
-    if (trovato === false ){
-        listaMail.push(mailUtente);
-        console.log('La tua mail non e presente, e stata aggiunta');
-    }
+// // // //  3.  stampa un messaggio appropriato sull’esito del controllo.
+//     if (trovato === false ){
+//         listaMail.push(mailUtente);
+//         console.log('La tua mail non e presente, e stata aggiunta');
+//     }
 
     
 
 
 // SECONDO METODO
 
-// // //  2.  controlla che sia nella lista di chi può accedere,
+// 2.  controlla che sia nella lista di chi può accedere,
+if (listaMail.includes(mailUtente)) {
 
-//     if (listaMail.includes(mailUtente)) {
+    // 3.  stampa un messaggio appropriato sull’esito del controllo.
+    console.log('La tua mail e gia presente');
 
-// // //  3.  stampa un messaggio appropriato sull’esito del controllo.
-//         console.log('La tua mail e gia presente');
+} else {
 
-//     } else {
+    //  3.  stampa un messaggio appropriato sull’esito del controllo.
+    listaMail.push(mailUtente);
 
-// // //  3.  stampa un messaggio appropriato sull’esito del controllo.
-//         listaMail.push(mailUtente);
-//         console.log('La tua mail non e presente, e stata aggiunta');
-// }
+    console.log('La tua mail non e presente, e stata aggiunta');
 
-//         console.log(listaMail);
+}
+
+console.log(listaMail);
+
 
 
 
